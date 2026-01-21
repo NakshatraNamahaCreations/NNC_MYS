b55539780c7f028df5b7bc564806e07e58e490a6
NakshatraNamahaCreations <ops.nnc@gmail.com>
2026-01-21 10:33:20 +0530

    Initiated

---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="48hCSxJt__qnJtCjeOTyEYf9WLZmNdrOqCol9yUh80g"
+        />
+      </Head>
+
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
